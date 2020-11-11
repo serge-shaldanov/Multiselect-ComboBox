20e709f8be2f5b190dc1dac1641dd18e14c0447e
Sergey Shaldanov <sergey.shaldanov@gmail.com>
2020-11-11 22:25:30 +0300

    Updated Nuget package build script.

--- MultiSelectComboBox/Sdl.MultiSelectComboBox/Create-Nuget-Package.bat
+++ MultiSelectComboBox/Sdl.MultiSelectComboBox/Create-Nuget-Package.bat
@@ -1,3 +1,3 @@
 @echo off
 
-nuget pack Sdl.MultiSelectComboBox.nuspec -Version 1.0.1813 -properties Id=Sdl.MultiSelectComboBox.Net46;Title=Sdl.MultiSelectComboBox.Net46;Description="Fork of Sdl.MultiSelectComboBox with .Net 4.6 support. Removed item grouping feature as it is not supported in .Net 4.6. I also added the ability to create selection items on the fly (see EnableNewItemsCreation and ItemFactoryService) properties"
+nuget pack Sdl.MultiSelectComboBox.nuspec -Version 1.0.1814 -properties Id=Sdl.MultiSelectComboBox.Net46;Title=Sdl.MultiSelectComboBox.Net46;Description="Fork of Sdl.MultiSelectComboBox with .Net 4.6 support. Removed item grouping feature as it is not supported in .Net 4.6. I also added the ability to create selection items on the fly (see EnableNewItemsCreation and ItemFactoryService) properties"
